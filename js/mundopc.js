ed8df80967118460611c947b42c202251093a18a
Lautaro Mónaco <lauti.monaco@hotmail.com>
2017-11-10 04:36:51 +0200

    Add files via upload

--- js/mundopc.js
+++ js/mundopc.js
@@ -30,7 +30,13 @@ $(".accordion").on("click", ".accordion-header", function() {
 
 });
 
-
+var waypoint = new Waypoint({
+  element: document.getElementById('productos'),
+  handler: function(direction) {
+    $('.box').addClass('animated fadeInUp')
+  },
+	offset: 450
+})
 
 var waypoint = new Waypoint({
   element: document.getElementById('menuconsulta'),
@@ -62,3 +68,4 @@ var waypoint = new Waypoint({
     $('.acordeon2').addClass('animated bounceIn')
   },
 })
+
